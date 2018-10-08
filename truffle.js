712fe2b5c6191aaac237371822ef4f95152c9c94
Hemang Thakur <18026025+Hemang999@users.noreply.github.com>
2018-10-08 21:20:38 +0530

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -4,14 +4,17 @@ module.exports =
   // for more about customizing your Truffle configuration!
   networks: 
   {
+
+    //Network configuration for Ganache RPC
     development: 
     {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
     },
-    
-    ourTestNet: 
+
+    //Network configuration for default private testnet
+    ourTestNet:                
     {
       host: "127.0.0.1",
       port: 8545,
